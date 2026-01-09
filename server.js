c61d40983f6e9da138ccd627132c7b190af2866f
corneliuspam <corneliuspam8736@gmail.com>
2026-01-09 09:32:29 +0100

    Update server.js

--- server.js
+++ server.js
@@ -20,6 +20,10 @@ app.use(express.static(path.join(__dirname, "public")));
 app.use(express.json());
 app.use(fileUpload());
 
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "index.html"));
+});
+
 // ===== Registration =====
 app.post("/register", async (req, res) => {
   const { username, email, phone, password } = req.body;
